850b46715bc2d3e898ac2fcdc4e1b649ef8f2fed
Alexander <alexskvr03@gmail.com>
2023-01-09 22:07:52 +0300

    rust game example

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,32 @@
+use std::{cmp::Ordering, io};
+
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    loop {
+        println!("Enter the number: ");
+        let mut user_string = String::new();
+
+        io::stdin().read_line(&mut user_string).expect("Error!");
+
+        let user_string: u32 = match user_string.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid input");
+                continue;
+            }
+        };
+        
+
+        match user_string.cmp(&secret_number) {
+            Ordering::Equal => { 
+                println!("You win!");
+                break;
+            },
+
+            Ordering::Greater => println!("Your number is greater than secret"),
+            Ordering::Less => println!("Your number is less than secret"),
+        }
+    }
 }
