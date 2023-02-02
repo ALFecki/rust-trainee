80ce587cd722a97e580ccbf619cef426e8d7cce4
ALFecki <alexskvr03@gmail.com>
2023-02-02 17:37:07 +0300

    powers + printing + other fixes

--- equation/src/main.rs
+++ equation/src/main.rs
@@ -6,9 +6,9 @@ mod polynomial;
 
 fn main() {
 
-    let g = Polynomial::new("x^2 - 2x - 24");
+    let g = Polynomial::new("x^2 - 2x + 3x - 24").unwrap();
     println!("{g}");
-    let b = Polynomial::new("y^2 + 6 - 7y");
+    let b = Polynomial::new("y^3 + y^2 + 6 - 7y").unwrap();
     println!("{b}");
     let c = g * b;
 
--- equation/src/polynomial.rs
+++ equation/src/polynomial.rs
@@ -17,60 +17,59 @@ pub struct Polynomial {
 }
 
 impl Polynomial {
-    pub fn new(expr: &str) -> Self {
-        Polynomial {
+    pub fn new(expr: &str) -> Result<Self, &str> {
+        Ok( Polynomial {
             terms: match Self::parse_terms(expr) {
                 Ok(res) => res,
                 Err(str) => {
-                    panic!("Error: {}", str);
+                    return Err(str);
                 }
             },
-        }
+        })
     }
 
     fn parse_terms(expr: &str) -> Result<BTreeMap<i32, f64>, &str> {
         let mut coeffs = BTreeMap::<i32, f64>::new();
-
         for caps in REGEX.captures_iter(expr) {
             if let Some(cap) = caps.name("first") {
-                let number = match Self::get_term_number(cap.clone().as_str(), false) {
+                let number = match Self::get_term_number(cap.as_str(), false) {
                     Ok(res) => res,
-                    Err(str) => return Err(&str),
+                    Err(str) => return Err(str),
                 };
                 coeffs
                     .entry(match Self::get_power(cap.as_str()) {
                         Ok(res) => res,
-                        Err(str) => return Err(&str),
+                        Err(str) => return Err(str),
                     })
-                    .and_modify(| num| *num += number)
+                    .and_modify(|num| *num += number)
                     .or_insert(number);
             } else if let Some(cap) = caps.name("second") {
                 let number = match Self::get_term_number(cap.as_str(), false) {
                     Ok(res) => res,
-                    Err(str) => return Err(&str),
+                    Err(str) => return Err(str),
                 };
                 coeffs
                     .entry(1)
-                    .and_modify(| num| *num += number)
+                    .and_modify(|num| *num += number)
                     .or_insert(number);
             } else if let Some(cap) = caps.name("third") {
                 let number = match Self::get_term_number(cap.as_str(), true) {
                     Ok(res) => res,
-                    Err(str) => return Err(&str),
+                    Err(str) => return Err(str),
                 };
                 coeffs
                     .entry(0)
-                    .and_modify(| num| *num += number)
+                    .and_modify(|num| *num += number)
                     .or_insert(number);
             }
         }
 
-        return Ok(coeffs);
+        Ok(coeffs)
     }
 
     fn get_power(cap: &str) -> Result<i32, &str> {
         let power_start = cap.find(|c: char| c == '^').unwrap_or(0) + 1;
-        let power_end = cap.find(|c: char| c == ' ').unwrap_or(cap.chars().count());
+        let power_end = cap.chars().count();
         return match cap
             .get(power_start..power_end)
             .unwrap_or("1")
@@ -102,7 +101,7 @@ impl Polynomial {
         } else {
             res += coeff;
         }
-        return Ok(res);
+        Ok(res)
     }
 }
 
@@ -110,9 +109,9 @@ impl Add for Polynomial {
     type Output = Polynomial;
 
     fn add(self, rhs: Self) -> Self::Output {
-        let mut new_map = self.terms.clone();
+        let mut new_map = self.terms;
         for term in rhs.terms {
-            new_map.entry(term.0).and_modify(| num| *num += term.1);
+            new_map.entry(term.0).and_modify(|num| *num += term.1);
         }
         Polynomial { terms: new_map }
     }
@@ -121,9 +120,9 @@ impl Add for Polynomial {
 impl Sub for Polynomial {
     type Output = Polynomial;
     fn sub(self, rhs: Self) -> Self::Output {
-        let mut new_map = self.terms.clone();
+        let mut new_map = self.terms;
         for term in rhs.terms {
-            new_map.entry(term.0).and_modify(| num| *num -= term.1);
+            new_map.entry(term.0).and_modify(|num| *num -= term.1);
         }
         Polynomial { terms: new_map }
     }
@@ -150,24 +149,41 @@ impl Div for Polynomial {
     type Output = (Polynomial, Option<(Polynomial, Polynomial)>);
 
     fn div(self, rhs: Self) -> Self::Output {
-
         todo!()
     }
 }
 
-impl Display for Polynomial {
+impl Display for Polynomial { // one huge crutch
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         let mut iter = self.terms.iter();
+        let mut is_first = true;
         while let Some(term) = iter.next_back() {
+            if *term.1 == 0.0 {
+                is_first = false;
+                continue;
+            }
             if *term.0 == 1 {
-                write!(f, "{:+}x ", term.1)?;
-                continue;
-            }
-            if *term.0 == 0 {
+                if *term.1 == 1.0 && is_first {
+                    write!(f, "x ")?;
+                } else if *term.1 == 1.0 {
+                    write!(f, "+x ")?;
+                } else if *term.1 == -1.0 {
+                    write!(f, "-x ")?;
+                } else {
+                    write!(f, "{:+}x ", term.1)?;
+                }
+            } else if *term.0 == 0 {
                 write!(f, "{:+} ", term.1)?;
-                continue;
+            } else if *term.1 == 1.0 && is_first {
+                write!(f, "x^{} ", term.0)?;
+            } else if *term.1 == 1.0 {
+                write!(f, "+x^{} ", term.0)?;
+            } else if *term.1 == -1.0 {
+                write!(f, "-x^{} ", term.0)?;
+            } else {
+                write!(f, "{:+}x^{} ", term.1, term.0)?;
             }
-            write!(f, "{:+}x^{} ", term.1, term.0)?;
+            is_first = false;
         }
         write!(f, "")
     }
