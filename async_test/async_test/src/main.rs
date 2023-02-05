086097d0dd1096a941522451279c3fb641bd178c
ALFecki <alexskvr03@gmail.com>
2023-02-06 01:25:56 +0300

    async is done

--- async_test/async_test/src/main.rs
+++ async_test/async_test/src/main.rs
@@ -1,3 +1,6 @@
+use futures::future::join_all;
+use futures::stream::FuturesUnordered;
+use futures::StreamExt;
 use std::time::Duration;
 
 async fn get_image_id(name: &str) -> i32 {
@@ -5,24 +8,39 @@ async fn get_image_id(name: &str) -> i32 {
     name.len() as i32
 }
 
+async fn task1(names: Vec<&'static str>) {
+    let mut handles = vec![];
+    for name in names {
+        let handle = get_image_id(name);
+        // let handle = tokio::spawn( async {
+        //     get_image_id(name).await
+        // });
+        handles.push(handle);
+    }
+    // for handle in handles {
+    //     println!("{}", handle.await);
+    // }
+    let results = join_all(handles.into_iter()).await;
+    for res in results {
+        println!("{res}",);
+    }
+}
+
+async fn task2(names: Vec<&'static str>) {
+    let mut futures = FuturesUnordered::new();
+    for name in names {
+        let handle = tokio::spawn(async { get_image_id(name).await });
+        futures.push(handle);
+    }
+    if let Some(Ok(result)) = futures.next().await {
+            println!("Get data {result}");
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), ()> {
-    let _names = vec!["first_name", "sname", "first_name_and_last_name"];
-    let a = tokio::spawn( async {
-        get_image_id("first_name").await
-    });
-
-    let b = tokio::spawn( async {
-        get_image_id("sname").await
-    });
-    let c = tokio::spawn( async {
-        get_image_id("first_name_and_last_name").await
-    });
-    println!("{:?}", b.await);
-
-    println!("{:?}", a.await);
-    println!("{:?}", c.await);
-
-
+    let names = vec!["first_name", "sname", "first_name_and_last_name"];
+    task1(names.clone()).await;
+    task2(names).await;
     Ok(())
 }
