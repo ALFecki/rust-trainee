8f4ff06fac502fbf4803c77ff33d27ee6b25a04c
ALFecki <alexskvr03@gmail.com>
2023-02-06 10:03:18 +0300

    async fix

--- async_test/async_test/src/main.rs
+++ async_test/async_test/src/main.rs
@@ -1,7 +1,5 @@
-use futures::future::join_all;
-use futures::stream::FuturesUnordered;
-use futures::StreamExt;
 use std::time::Duration;
+use tokio::sync::mpsc::channel;
 
 async fn get_image_id(name: &str) -> i32 {
     tokio::time::sleep(Duration::from_millis((name.len() as u64) * 100)).await;
@@ -11,29 +9,30 @@ async fn get_image_id(name: &str) -> i32 {
 async fn task1(names: Vec<&'static str>) {
     let mut handles = vec![];
     for name in names {
-        let handle = get_image_id(name);
-        // let handle = tokio::spawn( async {
-        //     get_image_id(name).await
-        // });
+        let handle = tokio::spawn(async { get_image_id(name).await });
         handles.push(handle);
     }
-    // for handle in handles {
-    //     println!("{}", handle.await);
-    // }
-    let results = join_all(handles.into_iter()).await;
-    for res in results {
-        println!("{res}",);
+    for handle in handles {
+        if let Ok(res) = handle.await {
+            println!("{res}");
+        }
     }
 }
 
 async fn task2(names: Vec<&'static str>) {
-    let mut futures = FuturesUnordered::new();
+    let (tx, mut rx) = channel(5);
+
+    let mut handles = vec![];
+
     for name in names {
-        let handle = tokio::spawn(async { get_image_id(name).await });
-        futures.push(handle);
+        let tx_cpy = tx.clone();
+        let handle = tokio::spawn(async move {
+            tx_cpy.send(get_image_id(name).await).await;
+        });
+        handles.push(handle);
     }
-    if let Some(Ok(result)) = futures.next().await {
-            println!("Get data {result}");
+    if let Some(res) = rx.recv().await {
+        println!("{}", res);
     }
 }
 
@@ -42,5 +41,6 @@ async fn main() -> Result<(), ()> {
     let names = vec!["first_name", "sname", "first_name_and_last_name"];
     task1(names.clone()).await;
     task2(names).await;
+
     Ok(())
 }
