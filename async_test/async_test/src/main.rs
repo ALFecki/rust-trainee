b81f26d9cdfb2388f150d1603176d6094e2f74fb
ALFecki <alexskvr03@gmail.com>
2023-02-08 12:34:36 +0300

    smth like sleep

--- async_test/async_test/src/main.rs
+++ async_test/async_test/src/main.rs
@@ -1,41 +1,96 @@
-use std::time::Duration;
-use tokio::select;
-use tokio::sync::mpsc::channel;
-use tokio::task::JoinSet;
+use std::future::Future;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+use std::task::Poll::{Pending, Ready};
+use tokio::time::Instant;
+use tokio::time::Duration;
 
-async fn get_image_id(name: &str) -> i32 {
-    tokio::time::sleep(Duration::from_millis((name.len() as u64) * 100)).await;
-    name.len() as i32
+pub fn my_sleep(duration: Duration) -> Delay {
+    Delay {
+        duration,
+    }
 }
 
-async fn task1(names: Vec<&'static str>) {
-    let mut handles = vec![];
-    for name in names {
-        let handle = tokio::spawn(async { get_image_id(name).await });
-        handles.push(handle);
-    }
-    for handle in handles {
-        if let Ok(res) = handle.await {
-            println!("{res}");
+pub struct Delay {
+    duration: Duration,
+}
+
+impl Future for Delay {
+    type Output = ();
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let check = self.duration;
+        let instant = Instant::now();
+        instant.checked_add(check);
+        if instant.elapsed().as_secs() != check.as_secs() {
+            return Pending;
         }
+        Ready(())
+
     }
 }
+
+
+
+
+
+
+async fn get_image_id(name: &str) -> usize {
+    tokio::time::sleep(std::time::Duration::from_millis((name.len() as u64) * 100)).await;
+    name.len()
+}
+
+async fn get_static_image() -> usize { 1 }
+
+// async fn task1(names: Vec<&'static str>) {
+//     let mut handlers: Vec<_>  = vec![];
+//     handlers = names.iter().map(|name| get_image_id(name)).collect();
+//     handlers.push(get_static_image());
+//
+//
+//     for handler in handlers {
+//         let res = handler.await;
+//         println!("{res}")
+//     }
+// }
 
 async fn task2(names: Vec<&'static str>) {
-    let mut set = JoinSet::new();
-    for name in names {
-        set.spawn(async { get_image_id(name).await});
-    }
-    if let Some(Ok(res)) = set.join_next().await {
-        println!("{res}");
-    }
+
+    // let mut handlers = vec![];
+    // let mut vec = vec![];
+    // for name in names {
+    //     handlers.push( tokio::spawn(async {get_image_id(name)}));
+    // }
+    // for handler in handlers {
+    //     vec.push(handler.await);
+    // }
+    // select! {
+    //     Some(Ok(val)) = vec.iter().next() => {
+    //
+    //     }
+    // }
+
+
+
+
+
+    // let mut set = JoinSet::new();
+    // for name in names {
+    //     set.spawn(async { get_image_id(name).await});
+    // }
+    // if let Some(Ok(res)) = set.join_next().await {
+    //     println!("{res}");
+    // }
+
 }
 
 #[tokio::main]
-async fn main() -> Result<(), ()> {
-    let names = vec!["first_name", "sname", "first_name_and_last_name"];
-    task1(names.clone()).await;
-    task2(names).await;
-
-    Ok(())
+async fn main() {
+    // let names = vec!["first_name", "sname", "first_name_and_last_name"];
+    // task1(names.clone()).await;
+    // task2(names).await;
+    println!("hello");
+    let sleep = my_sleep(Duration::from_secs(2));
+    sleep.await;
+    println!("yes");
 }
