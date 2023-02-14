fa4cd6192370feed3393ed86ebfa7144ebf43229
ALFecki <alexskvr03@gmail.com>
2023-02-15 02:01:20 +0300

    custom counters added

--- actix-web-test/src/main.rs
+++ actix-web-test/src/main.rs
@@ -1,10 +1,19 @@
-use actix_web::web::Data;
+use actix_web::web::{Data, Query};
 use actix_web::{main, web, App, HttpResponse, HttpServer, Responder};
+use serde::Deserialize;
+use std::collections::HashMap;
 use std::sync::atomic::{AtomicU32, Ordering};
+use std::sync::{Arc, Mutex};
 
 struct Counters {
     counter: AtomicU32,
     delete_counter: AtomicU32,
+    custom_counters: Arc<Mutex<HashMap<String, AtomicU32>>>,
+}
+
+#[derive(Clone, Deserialize)]
+struct CustomCounterQuery {
+    name: Option<String>,
 }
 
 fn change_counters(incr: &AtomicU32, to_swap: &AtomicU32) -> u32 {
@@ -12,11 +21,26 @@ fn change_counters(incr: &AtomicU32, to_swap: &AtomicU32) -> u32 {
     incr.fetch_add(1, Ordering::SeqCst) + 1
 }
 
-async fn counter_get(counters: Data<Counters>) -> impl Responder {
+async fn counter_get(counters: Data<Counters>, info: Query<CustomCounterQuery>) -> impl Responder {
     let temp = change_counters(&counters.counter, &counters.delete_counter);
-    println!("Counter is {}", temp);
-    println!("Delete counter is reset");
-    HttpResponse::Ok().body(format!("Now counter is {}, delete counter reset", temp))
+    let mut response = format!("Now counter is {}, delete counter reset", temp);
+
+    if let Some(name) = &info.name {
+        let mutex = counters.custom_counters.clone();
+        let mut map = mutex.lock().unwrap();
+
+        map.entry(name.clone())
+            .and_modify(|c| {
+                c.fetch_add(1, Ordering::SeqCst);
+            })
+            .or_insert(AtomicU32::new(1));
+
+        if let Some(val) = map.get_key_value(name) {
+            response += format!(", custom counter ({}) is {:?}", name, val.1).as_str()
+        }
+    }
+    println!("{}", response);
+    HttpResponse::Ok().body(response)
 }
 
 async fn counter_delete(counters: Data<Counters>) -> impl Responder {
@@ -32,7 +56,9 @@ async fn main() -> std::io::Result<()> {
     let counters: Data<Counters> = Data::new(Counters {
         counter: AtomicU32::new(0),
         delete_counter: AtomicU32::new(0),
+        custom_counters: Arc::new(Mutex::new(HashMap::new())),
     });
+
     HttpServer::new(move || {
         App::new()
             .app_data(Data::clone(&counters))
