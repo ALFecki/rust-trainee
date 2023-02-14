41b8ad1255d4129ec88c39cc076a155e208725da
ALFecki <alexskvr03@gmail.com>
2023-02-14 21:53:13 +0300

    actix-web task

--- actix-web-test/src/main.rs
+++ actix-web-test/src/main.rs
@@ -1,37 +1,41 @@
-
 use actix_web::web::Data;
-use actix_web::{main, App, HttpResponse, HttpServer, Responder, web};
-use std::sync::Mutex;
+use actix_web::{main, web, App, HttpResponse, HttpServer, Responder};
+use std::sync::atomic::{AtomicU32, Ordering};
 
-
-async fn counter_get(counter: Data<Mutex<u32>>) -> impl Responder {
-    let mut local_counter = match counter.lock() {
-        Ok(c) => c,
-        Err(_) => return HttpResponse::BadRequest().body("Mutex error")
-    };
-    if let Some(val) = local_counter.checked_add(1) {
-        *local_counter = val;
-    }
-    println!("Counter is {}", local_counter);
-    HttpResponse::Ok().body(format!("Now counter is {}", local_counter))
+struct Counters {
+    counter: AtomicU32,
+    delete_counter: AtomicU32,
 }
 
-async fn counter_delete(counter: Data<Mutex<u32>>) -> impl Responder {
-    let mut local_counter= match counter.lock() {
-        Ok(c) => c,
-        Err(_) => return HttpResponse::BadRequest().body("Mutex error")
-    };
-    *local_counter = 0;
-    println!("Counter is {}", local_counter);
-    HttpResponse::Ok().body("Counter reset")
+fn change_counters(incr: &AtomicU32, to_swap: &AtomicU32) -> u32 {
+    to_swap.swap(0, Ordering::SeqCst);
+    incr.fetch_add(1, Ordering::SeqCst) + 1
+}
+
+async fn counter_get(counters: Data<Counters>) -> impl Responder {
+    let temp = change_counters(&counters.counter, &counters.delete_counter);
+    println!("Counter is {}", temp);
+    println!("Delete counter is reset");
+    HttpResponse::Ok().body(format!("Now counter is {}, delete counter reset", temp))
+}
+
+async fn counter_delete(counters: Data<Counters>) -> impl Responder {
+    let temp = change_counters(&counters.delete_counter, &counters.counter);
+    println!("Counter is reset");
+    println!("Delete counter is {}", temp);
+
+    HttpResponse::Ok().body(format!("Counter reset, delete counter is {}", temp))
 }
 
 #[main]
 async fn main() -> std::io::Result<()> {
-    let counter: Data<Mutex<u32>> = Data::new(Mutex::new(0));
+    let counters: Data<Counters> = Data::new(Counters {
+        counter: AtomicU32::new(0),
+        delete_counter: AtomicU32::new(0),
+    });
     HttpServer::new(move || {
         App::new()
-            .app_data(Data::clone( &counter))
+            .app_data(Data::clone(&counters))
             .route("/counter", web::get().to(counter_get))
             .route("/counter", web::delete().to(counter_delete))
     })
