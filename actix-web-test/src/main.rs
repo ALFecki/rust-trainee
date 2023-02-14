09c88bf6f2f22e9258e72d92c102fa508d123db3
ALFecki <alexskvr03@gmail.com>
2023-02-14 17:11:14 +0300

    actix-web task

--- actix-web-test/src/main.rs
+++ actix-web-test/src/main.rs
@@ -1,12 +1,14 @@
+
 use actix_web::web::Data;
-use actix_web::{delete, get, main, App, HttpResponse, HttpServer, Responder, web};
-use std::sync::{Arc, Mutex, RwLock};
+use actix_web::{main, App, HttpResponse, HttpServer, Responder, web};
+use std::sync::Mutex;
 
-// #[get("/counter")]
+
 async fn counter_get(counter: Data<Mutex<u32>>) -> impl Responder {
-    println!("get");
-
-    let mut local_counter = counter.lock().unwrap();
+    let mut local_counter = match counter.lock() {
+        Ok(c) => c,
+        Err(_) => return HttpResponse::BadRequest().body("Mutex error")
+    };
     if let Some(val) = local_counter.checked_add(1) {
         *local_counter = val;
     }
@@ -14,21 +16,24 @@ async fn counter_get(counter: Data<Mutex<u32>>) -> impl Responder {
     HttpResponse::Ok().body(format!("Now counter is {}", local_counter))
 }
 
-// #[delete("/counter")]
-// async fn counter_delete(counter: Data<Cell<u32>>) -> impl Responder {
-//     counter.set(0);
-//     println!("Counter is {}", counter.get());
-//     HttpResponse::Ok().body("Counter reset")
-// }
+async fn counter_delete(counter: Data<Mutex<u32>>) -> impl Responder {
+    let mut local_counter= match counter.lock() {
+        Ok(c) => c,
+        Err(_) => return HttpResponse::BadRequest().body("Mutex error")
+    };
+    *local_counter = 0;
+    println!("Counter is {}", local_counter);
+    HttpResponse::Ok().body("Counter reset")
+}
 
 #[main]
 async fn main() -> std::io::Result<()> {
     let counter: Data<Mutex<u32>> = Data::new(Mutex::new(0));
-
     HttpServer::new(move || {
         App::new()
             .app_data(Data::clone( &counter))
             .route("/counter", web::get().to(counter_get))
+            .route("/counter", web::delete().to(counter_delete))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
