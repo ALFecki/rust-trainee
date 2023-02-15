0bd96ebbae6adce97652349b60e251919773f56e
ALFecki <alexskvr03@gmail.com>
2023-02-15 20:37:24 +0300

    counter to add

--- actix-web-test/src/main.rs
+++ actix-web-test/src/main.rs
@@ -24,44 +24,60 @@ impl Default for Counters {
 
 #[derive(Deserialize, Debug)]
 struct CustomCounterQuery {
-    na: Option<String>,
-    me: Option<String>,
+    n: String,
+    a: String,
+    m: String,
+    e: String,
 }
 
-fn change_counters(incr: &AtomicU32, to_swap: &AtomicU32) -> u32 {
+#[derive(Deserialize)]
+struct CustomAdd {
+    counter: u32,
+}
+
+fn change_counters(incr: &AtomicU32, to_swap: &AtomicU32, to_add: u32) -> u32 {
     to_swap.swap(0, Ordering::SeqCst);
-    incr.fetch_add(1, Ordering::SeqCst) + 1
+    incr.fetch_add(to_add, Ordering::SeqCst) + to_add
 }
 
 async fn counter_get(
     counters: Data<Counters>,
-    params: Query<HashMap<String, String>>,
+    params: Option<Query<CustomCounterQuery>>,
+    counter: Option<Query<CustomAdd>>,
 ) -> impl Responder {
-    let temp = change_counters(&counters.counter, &counters.delete_counter);
-    let mut response = format!("Now counter is {}, delete counter reset", temp);
-    if params.len() < 4 {
+    let mut response = String::new();
+    let mut to_add = match counter {
+        Some(val) => val.counter,
+        None => 1,
+    };
+    if let Some(params) = params {
         let mut name = String::new();
-        response += ", custom counter ";
         let mutex = counters.custom_counters.clone();
         let mut map = mutex.lock().await;
-        for param in params.0 {
-            name.push_str(param.1.as_str());
-        }
+
+        name.push_str(&params.n);
+        name.push_str(&params.a);
+        name.push_str(&params.m);
+        name.push_str(&params.e);
+
         map.entry(name.clone())
             .and_modify(|c| {
-                c.fetch_add(1, Ordering::SeqCst);
+                c.fetch_add(to_add, Ordering::SeqCst);
             })
-            .or_insert(AtomicU32::new(1));
+            .or_insert(AtomicU32::new(to_add));
+        to_add = 1;
         if let Some(val) = map.get_key_value(&name) {
-            response += format!(" ({}) is {:?}", name, val.1).as_str()
+            response += format!(", custom counter  ({}) is {:?}", name, val.1).as_str()
         }
     }
+    let temp = change_counters(&counters.counter, &counters.delete_counter, to_add);
+    response = format!("Now counter is {}, delete counter reset {}", temp, response);
     println!("{}", response);
     HttpResponse::Ok().body(response)
 }
 
 async fn counter_delete(counters: Data<Counters>) -> impl Responder {
-    let temp = change_counters(&counters.delete_counter, &counters.counter);
+    let temp = change_counters(&counters.delete_counter, &counters.counter, 1);
     println!("Counter is reset");
     println!("Delete counter is {}", temp);
 
