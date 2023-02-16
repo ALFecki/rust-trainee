9a32c6741f93e6f1064c3e3b5347e94911edbe7a
ALFecki <alexskvr03@gmail.com>
2023-02-16 17:52:53 +0300

    error handling

--- actix-web-test/src/xml.rs
+++ actix-web-test/src/xml.rs
@@ -1,27 +1,31 @@
 use actix_web::body::EitherBody;
 use actix_web::http::header::ContentType;
-use actix_web::{HttpRequest, HttpResponse, Responder};
-use serde::{Serialize, Serializer};
+use actix_web::{HttpRequest, HttpResponse, Responder, ResponseError};
+use serde::Serialize;
+use serde_xml_rs::Error;
+use std::fmt::{Display, Formatter};
 
-pub struct Xml<T>(pub T);
+#[derive(Debug)]
+enum XmlError {
+    SerializingError(Error),
+}
 
-impl<T> Serialize for Xml<T>
-where
-    T: Serialize,
-{
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        self.0.serialize(serializer)
+impl Display for XmlError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "XML serialize error")
     }
 }
+
+impl ResponseError for XmlError {}
+
+#[derive(Serialize)]
+pub struct Xml<T>(pub T);
 
 impl<T> Responder for Xml<T>
 where
     T: Serialize,
 {
-    type Body = EitherBody<String, String>;
+    type Body = EitherBody<String>;
 
     fn respond_to(self, _: &HttpRequest) -> HttpResponse<Self::Body> {
         return match serde_xml_rs::to_string(&self) {
@@ -31,16 +35,10 @@ where
                     .message_body(val)
                 {
                     Ok(respond) => respond.map_into_left_body(),
-                    Err(e) => HttpResponse::BadRequest()
-                        .message_body(e.to_string())
-                        .unwrap()
-                        .map_into_right_body(),
+                    Err(e) => HttpResponse::from_error(e).map_into_right_body(),
                 }
             }
-            Err(e) => HttpResponse::BadRequest()
-                .message_body(e.to_string())
-                .unwrap()
-                .map_into_right_body(),
+            Err(e) => HttpResponse::from_error(XmlError::SerializingError(e)).map_into_right_body(),
         };
     }
 }
