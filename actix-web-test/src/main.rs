601dbfee6fe553dc7859c015046a6b42fc2aa19a
ALFecki <alexskvr03@gmail.com>
2023-02-19 21:01:00 +0300

    content-type optimization not working

--- actix-web-test/src/main.rs
+++ actix-web-test/src/main.rs
@@ -1,15 +1,16 @@
+mod utilities;
 mod xml;
 
 use crate::xml::Xml;
-use actix_web::web::{Data, head, Json, Query};
-use actix_web::{main, web, App, Either, HttpRequest, HttpResponse, HttpServer, Responder, ResponseError};
+use actix_web::web::{Data, Json, Query};
+use actix_web::{main, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
-use serde::ser::SerializeStruct;
-use serde::{Deserialize, Serialize, Serializer};
+use crate::utilities::{change_counters, get_accept_header};
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::sync::atomic::{AtomicU32, Ordering};
 use std::sync::Arc;
-use actix_web::http::StatusCode;
+use actix_web::body::EitherBody;
 use tokio::sync::Mutex;
 
 #[derive(Default, Serialize)]
@@ -28,21 +29,32 @@ struct CustomCounterQuery {
     e: String,
 }
 
+struct ContentTypeResponse<T> where T: Clone {
+    data: T,
+    // request: HttpRequest
+}
+
+impl<T> Responder for ContentTypeResponse<T> where T: Clone
+{
+    type Body = EitherBody<String>;
+
+    fn respond_to(self, req: &HttpRequest) -> HttpResponse<Self::Body> {
+        let mut data_in_request = Json(self.data.clone()).respond_to(req);
+        if let Some(accept) = get_accept_header(&req) {
+            if let Ok(content_type) = accept.parse::<mime::Mime>() {
+                match content_type.type_() {
+                    mime::XML => data_in_request = Xml(self.data).respond_to(req),
+                    _ => data_in_request = Json(self.data).respond_to(req)
+                }
+            }
+        }
+        data_in_request
+    }
+}
+
 #[derive(Deserialize)]
 struct CustomAdd {
     counter: u32,
-}
-
-fn change_counters(incr: &AtomicU32, to_swap: &AtomicU32, to_add: u32) -> u32 {
-    to_swap.swap(0, Ordering::SeqCst);
-    incr.fetch_add(to_add, Ordering::SeqCst) + to_add
-}
-
-fn get_accept_header(req: &HttpRequest) -> Option<&str> {
-    match req.headers().get("accept") {
-        Some(accept) => accept.to_str().ok(),
-        None => None,
-    }
 }
 
 // fn create_response<T>(headers: HttpRequest, data: T) -> impl Responder
@@ -88,12 +100,11 @@ async fn counter_get(
     }
     change_counters(&counters.counter, &counters.delete_counter, to_add);
 
-    if let Some(accept) = get_accept_header(&headers) {
-        if accept == "application/xml" {
-            return Either::Left(Xml(counters));
-        }
-    };
-    Either::Right(Json(counters))
+    // ContentTypeResponse::new(Xml(counters), &headers)
+
+    ContentTypeResponse {
+        data: counters,
+    }
 }
 
 async fn counter_delete(counters: Data<Counters>) -> impl Responder {
@@ -117,40 +128,4 @@ async fn main() -> std::io::Result<()> {
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
-
-    // println!("{}", call(test, 1));
 }
-
-// fn call<Func, Args, Res>(function: Func, args: Args) -> Res
-// where
-//     Func: CallFunction<Args, Res>,
-//     Args: FromStr,
-// {
-//     function.call(args)
-// }
-//
-// pub trait CallFunction<Args, Res> {
-//     fn call(&self, args: Args) -> Res;
-// }
-//
-// impl<Func, Arg1, Res> CallFunction<Arg1, Res> for Func
-// where
-//     Func: Fn(Arg1) -> Res,
-// {
-//     fn call(&self, args: Arg1) -> Res {
-//         self(args)
-//     }
-// }
-//
-// impl<Func, Arg1, Arg2, Res> CallFunction<(Arg1, Arg2), Res> for Func
-// where
-//     Func: Fn(Arg1, Arg2) -> Res,
-// {
-//     fn call(&self, args: (Arg1, Arg2)) -> Res {
-//         self(args)
-//     }
-// }
-//
-// fn test(arg: i32) -> i32 {
-//     arg
-// }
